03d204abaa41a18770031c87931aee22f31f9987
Janos Pasztor <janos@pasztor.at>
2020-11-13 03:04:47 +0100

    Added PR count

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ def declare_variables(variables, macro):
 [![GitHub release (latest SemVer including pre-releases)](https://img.shields.io/github/v/release/containerssh/%s?include_prereleases&style=for-the-badge)](https://github.com/ContainerSSH/%s/releases)
 [![GitHub last commit](https://img.shields.io/github/last-commit/containerssh/%s?style=for-the-badge)](https://github.com/containerssh/%s)
 [![GitHub issues](https://img.shields.io/github/issues/ContainerSSH/%s?style=for-the-badge)](https://github.com/ContainerSSH/%s/issues)
+[![GitHub pull requests](https://img.shields.io/github/issues-pr/containerssh/%s?style=for-the-badge)](https://github.com/ContainerSSH/%s/pulls)
 [![Lint](https://img.shields.io/github/workflow/status/containerssh/%s/Lint?style=for-the-badge&label=Lint)](https://github.com/ContainerSSH/%s/actions?query=workflow%%3ALint)
 [![Tests](https://img.shields.io/github/workflow/status/containerssh/%s/Tests?style=for-the-badge&label=Tests)](https://github.com/ContainerSSH/%s/actions?query=workflow%%3ATests)
 [![CodeQL](https://img.shields.io/github/workflow/status/containerssh/%s/CodeQL?style=for-the-badge&label=CodeQL)](https://github.com/ContainerSSH/%s/actions?query=workflow%%3ACodeQL)
-[![Go Report Card](https://goreportcard.com/badge/github.com/containerssh/auth?style=for-the-badge)](https://goreportcard.com/report/github.com/containerssh/%s)
+[![Go Report Card](https://goreportcard.com/badge/github.com/containerssh/%s?style=for-the-badge)](https://goreportcard.com/report/github.com/containerssh/%s)
 [![LGTM Alerts](https://img.shields.io/lgtm/alerts/github/ContainerSSH/%s?style=for-the-badge)](https://lgtm.com/projects/g/ContainerSSH/%s/)
 """
-        return MD % (lib, lib, description, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib)
+        return MD % (lib, lib, description, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib, lib)
 
     @macro
     def since(version):
